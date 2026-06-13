449ea7753837f2f1bd2b77ec858b3a29b6377353
aenansh <231030317@juitsolan.in>
2026-06-14 00:26:27 +0530

    added user controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,57 @@
+import { User } from "../models/user.model.js";
+
 const signUpUser = async (req, res) => {
   try {
-    
-  } catch (error) {
-    
-  }
-}
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: "All fields required." });
+    }
 
-export { signUpUser };
+    const userExists = await User.findOne({
+      email,
+    });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ error: "A user with same e-mail already exists." });
+    }
+
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    return res.status(201).json({ message: "New user created." });
+  } catch (error) {
+    console.log("Failed to register user.", error);
+  }
+};
+
+const signInUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "All fields required." });
+    }
+
+    const userExists = await User.findOne({
+      email,
+    });
+
+    if (!userExists) {
+      return res
+        .status(400)
+        .json({ error: "New user detected, please sign up first." });
+    }
+
+    if (!(await userExists.isPasswordCorrect(password)))
+      return res.status(400).json({ error: "Incorrect password provided." });
+    return res.status(200).json({ message: "Welcome back user." });
+  } catch (error) {
+    console.log("Failed to login user.", error);
+  }
+};
+
+export { signUpUser, signInUser };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { signUpUser } from "../controllers/user.controller.js";
+import { signInUser, signUpUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/sign-up").post(signUpUser);
 router.route("/sign-in").post(signInUser);
-router.route("/sign-out").post(signOutUser);
-router.route("/current-user").get(getCurrentUser);
 
 export default router;
